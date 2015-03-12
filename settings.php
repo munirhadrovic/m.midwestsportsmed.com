a416a87e461cee6d6060ffeef1fd37cc4476a4c1
novicaa <novicamiketic@gmail.com>
2015-03-12 14:46:40 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Elk Grove Village Midwest Sports Medicine 901 W. Biesterfield Rd. Suite 300 Elk Grove Village, IL 60007 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=8779459090;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
